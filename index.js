394dc8a4e35cd58d5f35d9b19de6987f1b34e39a
vijayvrx8055 <vijay.kumar.vrx@gmail.com>
2024-12-28 18:46:13 +0530

    /tasks/add flow added

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const { resolve } = require('path');
-const cors = require('cors');
+let cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('static'));
+app.use(cors());
 
+let tasks = [
+  { "taskId": 1, "text": 'Fix bug #101', "priority": 2 },
+  { "taskId": 2, "text": 'Implement feature #202', "priority": 1 },
+  { "taskId": 3, "text": 'Write documentation', "priority": 3 }
+];
+
+//======================================================================
+app.get('/tasks/add',(req,res)=>{
+  let taskId = parseInt(req.query.taskId);
+  let text = req.query.text;
+  let priority = parseInt(req.query.priority);
+  let newTask = { "taskId": taskId, "text": text, "priority": priority };
+  addNewTask(newTask);
+  res.json({"tasks": tasks});
+});
+
+function addNewTask(newTask){
+  tasks.push(newTask);
+}
+//======================================================================
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
